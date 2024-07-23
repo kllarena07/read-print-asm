baeb72b526f35c368868b0c6be6b1b7ed37da9ee
Kieran Llarena <baymax709@gmail.com>
2024-07-22 22:33:28 -0400

    feat: recursively output user input

--- main.s
+++ main.s
@@ -9,7 +9,27 @@ _main:
   mov x16, #3
   svc 0
 
+  mov x9, #0
+  bl _print
   b _terminate
+
+
+_print:
+  mov x0, #1
+  adrp x1, buffer@PAGE
+  add x1, x1, buffer@PAGEOFF
+  add x1, x1, x9
+  mov x2, #1
+  mov x16, #4
+  svc 0
+
+  cmp x9, #4
+  bge end
+  add x9, x9, 1
+  bl _print
+
+end:
+  ret
 
 _terminate:
   mov x0, #0
